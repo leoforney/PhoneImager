fe239156f0db1b51733e4e8a889df1423ce47bab
Leo Forney <forneyleo@gmail.com>
2017-02-07 22:49:16 -0600

    Downloads files successfully

--- src/main/java/tk/leoforney/phoneimager/Main.java
+++ src/main/java/tk/leoforney/phoneimager/Main.java
@@ -1,17 +1,27 @@
 package tk.leoforney.phoneimager;
 
-import com.github.axet.vget.VGet;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.kohsuke.github.*;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Leo on 12/26/2016.
  */
 public class Main {
     private static String API_KEY = "8534df699907e4e673e9ff932af2acdfb4c33056";
+    private static String username;
+    private static String password;
 
     private static int stage = 0;
 
@@ -63,18 +73,23 @@ public class Main {
                 }
             } if (stage == 2) {
                 assets = selectedRepo.listReleases().asList().get(0).getAssets();
-                log("Assets for the latest release of " +selectedRepo.getGitTransportUrl());
+                log("Assets for the latest release of " + selectedRepo.getGitTransportUrl());
                 for (GHAsset asset: assets) {
                     log(asset.getName());
                 }
                 stage++;
             } if (stage == 3) {
-                File homeDirectory = new File(System.getProperty("user.home") + "/PhoneImagerTemp/");
+                File homeDirectory = new File(System.getProperty("user.home") + "/PhoneImager/");
                 log("Downloading all files to " + homeDirectory.getAbsolutePath());
                 for (GHAsset asset: assets) {
-                    new VGet(asset.getUrl(), new File(homeDirectory + asset.getName()));
+                    FileUtils.writeStringToFile(new File(homeDirectory + File.separator + asset.getName()),
+                            getFile(selectedRepo, asset.getId()));
                 }
-                terminate();
+                stage++;
+            }
+            if (stage == 4) {
+                System.out.println("Downloaded all files!");
+                stage++;
             }
             Thread.sleep(250);
         }
@@ -83,6 +98,26 @@ public class Main {
 
     private static void log(String logString) {
         System.out.println(logString);
+    }
+
+    private static String getFile(GHRepository repo, int id) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        URI uri = new URI("https://api.github.com/repos/" + repo.getFullName() + "/releases/assets/" + id + "?access_token=" + API_KEY);
+
+        HttpGet httpGet = new HttpGet(uri);
+        httpGet.setHeader("Accept", "application/octet-stream");
+
+        org.apache.http.HttpResponse response = httpClient.execute(httpGet);
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuilder result = new StringBuilder();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
     }
 
     private static void terminate() {stage = -1;}
