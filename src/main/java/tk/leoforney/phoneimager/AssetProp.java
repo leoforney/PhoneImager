68e1c90376de15ff4e3580ac510b32feec7754cf
Leo Forney <forneyleo@gmail.com>
2017-02-08 21:55:52 -0600

    Switched to maven and create tool to generate schema.json

---
+++ src/main/java/tk/leoforney/phoneimager/AssetProp.java
@@ -0,0 +1,10 @@
+package tk.leoforney.phoneimager;
+
+/**
+ * Created by Leo on 2/8/2017.
+ */
+public class AssetProp {
+    public String fileName;
+    public String destination;
+    public String type;
+}
--- src/main/java/tk/leoforney/phoneimager/Main.java
+++ src/main/java/tk/leoforney/phoneimager/Main.java
@@ -1,5 +1,6 @@
 package tk.leoforney.phoneimager;
 
+import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.apache.http.client.methods.HttpGet;
@@ -20,8 +21,7 @@ import java.util.Scanner;
  */
 public class Main {
     private static String API_KEY = "8534df699907e4e673e9ff932af2acdfb4c33056";
-    private static String username;
-    private static String password;
+    private static File homeDirectory = new File(System.getProperty("user.home") + "/PhoneImager/");
 
     private static int stage = 0;
 
@@ -42,17 +42,20 @@ public class Main {
         List<GHRepository> repositoryList = null;
         List<GHAsset> assets = null;
 
+        FileUtils.cleanDirectory(homeDirectory);
+
         while (Thread.currentThread().isAlive() && stage != -1) {
             if (stage == 0) { // Display repositories
                 repositoryList = new ArrayList<GHRepository>(organization.getRepositories().values());
 
                 log("Choose a repository to download by typing the number:");
 
-                for (GHRepository repository: repositoryList) {
+                for (GHRepository repository : repositoryList) {
                     log("[" + repositoryList.indexOf(repository) + "] " + repository.getFullName());
                 }
                 stage++;
-            } if (stage == 1) {
+            }
+            if (stage == 1) {
                 log("Selection: ");
                 String selectionString = scanner.next();
                 if (NumberUtils.isNumber(selectionString)) {
@@ -60,7 +63,7 @@ public class Main {
 
                     if (selectionNumber + 1 <= repositoryList.size()) {
                         selectedRepo = repositoryList.get(selectionNumber);
-
+                        assets = selectedRepo.listReleases().asList().get(0).getAssets();
                         stage++;
                     } else {
                         log("Incorrect input. Please retry.");
@@ -71,25 +74,69 @@ public class Main {
                     log("Incorrect input. Please retry.");
                     stage = 0;
                 }
-            } if (stage == 2) {
-                assets = selectedRepo.listReleases().asList().get(0).getAssets();
-                log("Assets for the latest release of " + selectedRepo.getGitTransportUrl());
-                for (GHAsset asset: assets) {
-                    log(asset.getName());
-                }
-                stage++;
-            } if (stage == 3) {
-                File homeDirectory = new File(System.getProperty("user.home") + "/PhoneImager/");
-                log("Downloading all files to " + homeDirectory.getAbsolutePath());
-                for (GHAsset asset: assets) {
-                    FileUtils.writeStringToFile(new File(homeDirectory + File.separator + asset.getName()),
-                            getFile(selectedRepo, asset.getId()));
+            }
+            if (stage == 2) {
+                for (GHAsset asset : assets) {
+                    System.out.println("Downloading " + asset.getName() + " from " + selectedRepo.listReleases().asList().get(0).getName());
+                    saveFile(asset);
                 }
                 stage++;
             }
-            if (stage == 4) {
+            if (stage == 3) {
                 System.out.println("Downloaded all files!");
-                stage++;
+                if (args.length >= 1 && args[0].equals("generate")) {
+                    stage = -100;
+                } else {
+                    for (File file : homeDirectory.listFiles()) {
+                        if (file.getName().equals("schema.json")) stage++;
+                    }
+                    System.out.println("This repository doesn't implement the schema.json mapping format... Please ask the developer to fix this");
+                    System.exit(101);
+                }
+
+            }
+            if (stage == -100) {
+                List<AssetProp> propList = new ArrayList<AssetProp>();
+                scanner.nextLine();
+                for (GHAsset asset : assets) {
+
+                    AssetProp prop = new AssetProp();
+                    prop.fileName = asset.getName();
+
+                    String contentType = asset.getContentType();
+
+                    if (contentType.equals("application/vnd.android.package-archive")) {
+                        prop.type = "apk";
+                    } else if (contentType.equals("text/xml") || contentType.equals("application/xml")) {
+                        prop.type = "hardwaremap";
+                    } else {
+                        prop.type = "unknown";
+                    }
+
+                    if (!prop.type.equals("hardwaremap")) {
+                        System.out.println("Should " + asset.getName() + " be on DS, RC, or BOTH?");
+                        String input = scanner.nextLine().toLowerCase();
+                        if (input.equals("rc") || input.equals("ds") || input.equals("both")) {
+                            prop.destination = input;
+                        } else {
+                            System.exit(103);
+                        }
+                    } else {
+                        prop.destination = "rc";
+                    }
+
+
+
+                    propList.add(prop);
+                }
+                String propListJson = new Gson().toJson(propList);
+                File schemajson = new File(homeDirectory + File.separator + "schema.json");
+                FileUtils.writeStringToFile(schemajson, propListJson);
+                System.out.println("Wrote " + schemajson.getPath());
+                System.exit(0);
+            }
+            if (stage == 4) {
+
             }
             Thread.sleep(250);
         }
@@ -100,9 +147,10 @@ public class Main {
         System.out.println(logString);
     }
 
-    private static String getFile(GHRepository repo, int id) throws Exception {
+    private static void saveFile(GHAsset asset) throws Exception {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        URI uri = new URI("https://api.github.com/repos/" + repo.getFullName() + "/releases/assets/" + id + "?access_token=" + API_KEY);
+        URI uri = new URI("https://api.github.com/repos/" + asset.getOwner().getFullName() + "/releases/assets/" +
+                asset.getId() + "?access_token=" + API_KEY);
 
         HttpGet httpGet = new HttpGet(uri);
         httpGet.setHeader("Accept", "application/octet-stream");
@@ -117,8 +165,10 @@ public class Main {
         while ((line = rd.readLine()) != null) {
             result.append(line);
         }
-        return result.toString();
+        FileUtils.writeStringToFile(new File(homeDirectory + File.separator + asset.getName()), result.toString());
     }
 
-    private static void terminate() {stage = -1;}
+    private static void terminate() {
+        stage = -1;
+    }
 }
